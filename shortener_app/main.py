7ceac8a51bb13c67f4e9cb4d5e04836f6faa7220
Jude Oyedele <judeokennywise@gmail.com>
2023-08-13 23:54:31 +0100

    resrtucture

--- shortener_app/main.py
+++ shortener_app/main.py
@@ -8,9 +8,27 @@ from fastapi.responses import RedirectResponse
 from . import crud, models, schemas
 from .config import get_settings
 from . import schemas
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import JSONResponse
 
 app = FastAPI()
 models.Base.metadata.create_all(bind=engine)
+
+origins = [
+    "http://localhost",
+    "http://localhost:8000",
+    "http://localhost:3000",
+    "http://localhost:8080",
+    "http://localhost:4200",
+]
+app.add_middleware(
+    CORSMiddleware,
+     allow_origins="*",
+      allow_credentials=True,
+       allow_methods=["*"],
+        allow_headers=["*"]
+)
+
 
 def raise_not_found(request):
     message = f"URL '{request.url}' doesn't exist"
@@ -51,12 +69,12 @@ def get_url_info(
 
 @app.get("/")
 def read_root():
-    return "Welcome to the URL shortener API :)"
+    return {"message":"Welcome to the URL shortener API :)"} 
 
 def raise_bad_request(message):
     raise HTTPException(status_code=400, detail=message)
 
-def get_admin_info(db_url: models.URL) -> schemas.URLInfo:
+def get_admin_info(db_url= models.URL) -> schemas.URLInfo:
     base_url = URL(get_settings().base_url)
     admin_endpoint = app.url_path_for(
         "administration info", secret_key=db_url.secret_key
@@ -69,7 +87,7 @@ def get_admin_info(db_url: models.URL) -> schemas.URLInfo:
 @app.post("/url", response_model=schemas.URLInfo)
 def create_url(url: schemas.URLBase, db: Session = Depends(get_db)):
     if not validators.url(url.target_url):
-        raise_bad_request(message="Your provided URL is not valid")
+        raise_bad_request(message="Your provided URL is not valid. Make sure to include 'http://' or 'https://' in the url")
 
     db_url = crud.create_db_url(db=db, url=url)
     return get_admin_info(db_url)
